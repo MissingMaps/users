62513c371a1acdfb7dd5c2055181c84997c90dbf
Marc Farra <marcfarra@gmail.com>
2016-01-11 11:14:35 -0500

    Add state sharing between parent container and children

--- src/containers/BadgeView.js
+++ src/containers/BadgeView.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import fetch from 'isomorphic-fetch';
 import BadgeBox from '../components/BadgeBox.js';
 
 export default React.createClass({
   getInitialState: function () {
     return {
-      badges: []
+      badges: this.props.user.badges || []
     };
   },
-  componentDidMount: function () {
-    let component = this;
-    if (process.env.NODE_ENV === 'development') {
-      fetch('/test/fixtures/db.json')
-      .then(function (response) {
-        if (response.status >= 400) {
-          throw new Error('Bad response');
-        }
-        return response.json();
-      })
-      .then(function (db) {
-        if (component.isMounted()) {
-          component.setState(db);
-        }
-      });
-    }
+  componentWillReceiveProps: function (nextProps) {
+    this.setState({
+      badges: nextProps.user.badges
+    });
   },
   render: function () {
     return <BadgeBox badges={this.state.badges} />;
--- src/containers/User.js
+++ src/containers/User.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import UserSubHead from '../components/UserSubHead';
+import fetch from 'isomorphic-fetch';
 
 export default React.createClass({
+  getInitialState: function () {
+    return {
+      user: {}
+    };
+  },
+  componentDidMount: function () {
+    let component = this;
+    if (process.env.NODE_ENV === 'development') {
+      fetch('/test/fixtures/db.json')
+      .then(function (response) {
+        if (response.status >= 400) {
+          throw new Error('Bad response');
+        }
+        return response.json();
+      })
+      .then(function (db) {
+        if (component.isMounted()) {
+          component.setState({
+            user: db
+          });
+        }
+      });
+    }
+  },
   render: function () {
     return (
       <div>
         <div id = "User-Container">
           <div id = "Main-User-Container">
             <UserSubHead username={this.props.params.id}/>
-            {this.props.children}
+            {(
+              (typeof this.state.user !== 'undefined')
+              ? this.props.children && React.cloneElement(this.props.children, {
+                user: this.state.user
+              })
+                : <div>Loading...</div>)
+            }
           </div>
         </div>
       </div>
