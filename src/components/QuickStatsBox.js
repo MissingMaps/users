24a13f7dfc672834f6518a7707291e56241501bf
Dylan <Dylan@developmentseed.org>
2016-01-12 16:25:10 -0500

    Initial push

--- src/components/QuickStatsBox.js
+++ src/components/QuickStatsBox.js
@@ -1,18 +1,6 @@
 import React from 'react';
 
-function stripWS (text) {
-  return text.replace(/ /g, '');
-}
-
 export default function QuickStatsBox (props) {
-	console.log(props);
-  var list = Object.keys(props.badges).map((badge) => {
-    console.log(props);
-    return (
-      <li key={stripWS(badge)}>
-      </li>
-    );
-  });
   return (
     <div className = "Quick-User-Stats-Container">
       <div className = "badgeheader">
