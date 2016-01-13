2cefea5dafc41eda6e109702148309230a219ad6
nbumbarger <nbumbarger@gmail.com>
2016-01-13 16:24:46 -0500

    Added responsive domain switching to chart

--- src/components/ContributionBox.js
+++ src/components/ContributionBox.js
@@ -4,12 +4,39 @@ import CalHeatMap from 'cal-heatmap';
 export default React.createClass({
   getInitialState: function () {
     return {
-      timestamps: this.props.timestamps || {}
+      timestamps: this.props.timestamps || {},
+      windowWidth: window.innerWidth,
+      domain: 'month',
+      range: 12,
+      subDomain: 'day',
+      rowLimit: 7,
+      domainLabelFormat: '%b',
+      cellSize: 12,
+      cal: new CalHeatMap()
     };
   },
+  handleResize: function (e) {
+    // Set the number of calendar months to display at the
+    // smaller of (horizontal resolution + 30) / 100 and 12.
+    this.setState({windowWidth: window.innerWidth});
+    var months = ~~((this.state.windowWidth + 30) / 100);
+    if (months > 12) months = 12;
+    this.setState({range: months});
+
+    var cal = this.state.cal;
+    cal.destroy();
+    document.getElementById('cal-heatmap').innerHTML = '';
+    this.initCal(new CalHeatMap());
+  },
   componentDidMount: function () {
+    window.addEventListener('resize', this.handleResize);
+    this.initCal(this.state.cal);
+  },
+  componentWillUnmount: function () {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  initCal: function (cal) {
     var data = this.props.timestamps;
-    var cal = new CalHeatMap();
     var parser = function (data) {
       var timestamps = {};
       data.forEach(function (ts, i) {
@@ -18,13 +45,13 @@ export default React.createClass({
       return timestamps;
     };
     cal.init({
-      domain: 'month',
-      subDomain: 'day',
-      cellSize: 15.25,
-      range: 12,
-      rowLimit: 7,
+      domain: this.state.domain,
+      range: this.state.range,
+      subDomain: this.state.subDomain,
+      rowLimit: this.state.rowLimit,
+      domainLabelFormat: this.state.domainLabelFormat,
+      cellSize: this.state.cellSize,
       start: new Date(data[0]),
-      // maxDate: var end_date = new Date(start_date.setDate(start_date.getDate() + (7 * 1)));,
       data: data,
       dataType: 'json',
       afterLoadData: parser
@@ -34,7 +61,7 @@ export default React.createClass({
     return (
       <div>
         <div className = "Contribute-Timeline-Container">
-          <div className = "badgeheader">Contribution Timeline</div>
+          <div className = "badgeheader">Contribution Timeline Current window width: {this.state.windowWidth}</div>
           <div className = "Contribute-Timeline-Content">
           <div id="cal-heatmap"></div>
           </div>
