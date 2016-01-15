c7a32731a43d54555d3eb158c32e8d0cbef071c2
nbumbarger <nbumbarger@gmail.com>
2016-01-15 17:05:16 -0500

    Added badges in progress (< level 1) to badge roll

--- src/containers/BadgeView.js
+++ src/containers/BadgeView.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import BadgeView from '../components/FullBadgeBox.js';
+import {getBadgeProgress} from '../badge_logic/badge_cruncher'
 
 export default React.createClass({
   getInitialState: function () {
     return {
-      badges: this.props.user.badges || []
+      earnedBadges: this.props.user.badges || [],
+      progress: {}
     };
   },
   componentWillReceiveProps: function (nextProps) {
+    var user = nextProps.user;
+    var progress = getBadgeProgress(user);
+
     this.setState({
-      badges: nextProps.user.badges
+      earnedBadges: nextProps.user.badges,
+      progress: progress
     });
   },
   render: function () {
     console.log(this.props);
-    return <BadgeView badges={this.state.badges} />;
+    return <BadgeView badges={this.state.earnedBadges} progress={this.state.progress}/>;
   }
 });
