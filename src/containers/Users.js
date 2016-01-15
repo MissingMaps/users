6df9a9d3d65c302342fff6b0166ab7e2dcfb52f9
Marc Farra <marcfarra@gmail.com>
2016-01-15 11:19:14 -0500

    Add API to users page

--- src/containers/Users.js
+++ src/containers/Users.js
@@ -1,29 +1,43 @@
 import React from 'react';
+import R from 'ramda';
 import SearchBar from 'react-search-bar';
 
 export default React.createClass({
   getInitialState: function () {
     return {
-      users: [
-        'leo',
-        'bill',
-        'barbara',
-        'christine',
-        'allie',
-        'lucille',
-        'glenn',
-        'jeanette',
-        'madge',
-        'winifred']
+      users: [],
+      names: []
     };
   },
+  componentDidMount: function () {
+    let component = this;
+    if (process.env.NODE_ENV === 'development') {
+      fetch('http://104.236.25.175:3000/users')
+      .then(function (response) {
+        if (response.status >= 400) {
+          throw new Error('Bad response');
+        }
+        return response.json();
+      })
+      .then(function (users) {
+        var names = R.map(R.prop('name'), users);
+        if (component.isMounted()) {
+          component.setState({
+            users: users,
+            names: names
+          });
+        }
+      });
+    }
+  },
   onChange: function (input, resolve) {
-    resolve(this.state.users.filter((suggestion) => {
+    resolve(this.state.names.filter((suggestion) => {
       return suggestion.toLowerCase().startsWith(input.toLowerCase());
     }));
   },
   onSubmit: function (input) {
-    this.props.history.push('/' + input);
+    var user = R.find(R.propEq('name', input))(this.state.users);
+    this.props.history.push('/' + user.id);
   },
 
   render: function () {
