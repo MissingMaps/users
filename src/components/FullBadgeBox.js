a7e7d078378723661494c1d224b0795140c299ab
Marc Farra <marcfarra@gmail.com>
2016-01-15 18:54:07 -0500

    Add some badge logic to give tasks

--- src/components/FullBadgeBox.js
+++ src/components/FullBadgeBox.js
@@ -26,25 +26,44 @@ function mapBadgeToImage (badge) {
 function mapBadgeToDescrip (badge) {
   var map = {
     'Road Builder': 'Map many roads across the land.',
-    'Road Maintainer': 'Update and correct misplaced roads.',
-    'Building Builder': 'Build 15 buildings.',
+    'Road Maintainer': 'Updated and corrected misplaced roads.',
+    'Building Builder': 'Built all those buildings!',
     'Consistentency': 'Mapped every day for a week!',
     'GPS Trace Creator': 'Uploaded GPS traces through their OSM',
     'JOSM User': 'Used JSOM to map an area',
-    'Long & Winding Road': 'Add 50km of roads.',
+    'Long & Winding Road': 'Created lots of roads.',
     'Long & Winding Road Maintainer': 'Maintaining those long, long roads.',
-    'Mapathoner': 'Unstoppable mapping mahcine. Map 10 days in a row.',
+    'Mapathoner': 'Unstoppable mapping machine.',
     'Node Builder': 'Added a truckful of nodes.',
     'TaskMan Scrutinizer': 'Scrutinize tasks with great vigilance',
     'TaskMan Square Champion': 'Mapped out 10 HOT task squares.',
-    'Waterway Creator': 'A regular mapping Poseidian, mapping many waterways.',
+    'Waterway Creator': 'A regular mapping Poseidon, mapping many waterways.',
     'World Renown': 'No land is outside your watchful gaze. Mapped in each continent.',
     'Year-long Mapper': 'You are so very dedicated. You have mapped for an entire year.',
-    'Point Creator': "You must really love points!"
+    'Point Creator': 'You must really love points!'
   };
   return map[badge];
 }
 
+function mapBadgeToTask (badge, x) {
+  var map = {
+      'Road Builder': (x) => `Add ${x} roads.`,
+      'Road Maintainer': (x) => `Modify ${x} roads.`,
+      'Building Builder': (x) => `Build ${x} buildings.`,
+      'Consistentency': (x) => 'Map every day for a week.',
+      'GPS Trace Creator': (x) => 'Upload more GPS traces through OSM',
+      'JOSM User': (x) => 'Use JSOM to map an area',
+      'Long & Winding Road': (x) => `Add ${x} km of roads.`,
+      'Long & Winding Road Maintainer': (x) => `Modify ${x} km of roads.`,
+      'Mapathoner': (x) => `Participate in ${x} in a row.`,
+      'Node Builder': (x) => `Add ${x} nodes.`,
+      'Waterway Creator': (x) => `Add ${x} km of waterways`,
+      'World Renown': (x) => `Map in ${x} different countries`,
+      'Year-long Mapper': (x) => `Map ${x} days.`,
+      'Point Creator': (x) => `Add ${x} points of interests`
+  };
+  return map[badge](x);
+}
 module.exports.descriptions = mapBadgeToDescrip;
 
 // Strips whitespace
@@ -81,7 +100,8 @@ export default (props) => {
     var badge = props.progress.all[val];
     return {
       'description': mapBadgeToDescrip(badge.name),
-      'progress': Math.floor(badge.points.percentage) + "% of the way to level " + badge.nextBadgeLevel,
+      'progress': Math.floor(badge.points.percentage) + "% of the way to level " + badge.nextBadgeLevel + '. ' +
+                  mapBadgeToTask(badge.name, Math.floor(badge.points.nextPoints - badge.points.currentPoints)),
       'name': badge.name
     }
   });
@@ -105,39 +125,39 @@ export default (props) => {
           </div>
         </div>
       </li>
-      )
+    )
 
   })
   return (
-  <div>
-    <div id = "Badge-Container">
-      <div className = "Badge-Box-Content">
-        <div className = "badgeroll-frame">
-          <div className = "badgeroll-center card">
-            <div className = "Card-title">
-            EARNED BADGES
+    <div>
+      <div id = "Badge-Container">
+        <div className = "Badge-Box-Content">
+          <div className = "badgeroll-frame">
+            <div className = "badgeroll-center card">
+              <div className = "Card-title">
+                EARNED BADGES
+              </div>
+              <ul className = "Badge-Roll">
+                {list}
+              </ul>
             </div>
-            <ul className = "Badge-Roll">
-              {list}
-            </ul>
+          </div>
+        </div>
+      </div>
+      <div id = "Badge-Container">
+        <div className = "Badge-Box-Content">
+          <div className = "badgeroll-frame">
+            <div className = "badgeroll-center card">
+              <div className = "Card-title">
+                BADGE PROGRESS
+              </div>
+              <ul className = "Badge-Roll">
+                {progressList}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div id = "Badge-Container">
-      <div className = "Badge-Box-Content">
-        <div className = "badgeroll-frame">
-          <div className = "badgeroll-center card">
-            <div className = "Card-title">
-              BADGE PROGRESS
-            </div>
-            <ul className = "Badge-Roll">
-              {progressList}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
   );
 };
