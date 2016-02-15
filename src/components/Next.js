e05dc34ae5614cfe18f73bc543eb9f95befb5bc2
Marc Farra <marcfarra@gmail.com>
2016-02-15 13:13:16 -0500

    Link + to badges

--- src/components/Next.js
+++ src/components/Next.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import {getBadgeProgress} from '../badge_logic/badge_cruncher.js';
 import BadgeInProgress from '../components/BadgeInProgress.js';
+import {Link} from 'react-router';
 
 export default (props) => {
   var progress = getBadgeProgress(props.data);
   var mostObtainableBadges = progress.mostAttainable;
+  if (!props.hasOwnProperty('data')) {
+    return <div></div>;
+  }
   return (
       <div className = "Split split-smbadges">
         <div className = "descriptor">Upcoming Badges</div>
         <div className = "Upcoming-Badges-Box">
           <BadgeInProgress badge={mostObtainableBadges[0]} badgeClass={'upcoming'}/>
           <BadgeInProgress badge={mostObtainableBadges[1]} badgeClass={'upcoming'}/>
-          <div className = "See-More-Badges">+</div>
+          <Link className = "See-More-Badges" to={`/${props.data.id}/badges`}>+</Link>
         </div>
         <div className="block-contribute">
           <p className="side-text">Up your badge game by continuing to contribute.</p>
