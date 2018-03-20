1967038645b60536aedf94560d8286235471d72c
Seth Fitzsimmons <seth@mojodna.net>
2018-03-20 11:52:12 -0700

    Remove username auto-complete
    This required loading all known users (a large payload). Instead, assume
    that the user knows what they're doing.

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import UserSubHead from '../components/UserSubHead';
-import {Link} from 'react-router';
-import Header from '../components/Header.js';
-import Footer from '../components/Footer.js';
 import fetch from 'isomorphic-fetch';
+import React from 'react';
+import { Link } from 'react-router';
+
+import UserSubHead from '../components/UserSubHead';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default React.createClass({
   getInitialState: function () {
@@ -12,85 +13,79 @@ export default React.createClass({
     };
   },
   componentDidMount: function () {
-    let component = this;
-    var fetch_thisid = this.props.params.name;
-    fetch(`https://osm-stats-api.azurewebsites.net/users`)
-      .then(function (response) {
+    const { params: { name } } = this.props;
+
+    fetch(`https://osm-stats-api.azurewebsites.net/users/${name}`)
+      .then(response => {
         if (response.status >= 400) {
-          throw new Error('Bad response Users Fetch');
+          throw new Error('Bad response User Fetch');
         }
+
         return response.json();
       })
-      .then(function (data) {
-        var usercheck = data.filter(function (element) {
-          return element.name.replace(/\s+/g, '-').toLowerCase() === fetch_thisid;
-        });
-
-        if (usercheck.length > 0) {
-          fetch(`https://osm-stats-api.azurewebsites.net/users/${usercheck[0].id}`)
-          .then(function (response) {
-            if (response.status >= 400) {
-              throw new Error('Bad response User Fetch');
-            }
-            return response.json();
-          })
-          .then(function (db) {
-            if (component.isMounted()) {
-              component.setState({
-                user: db
-              });
-            }
-          });
-        } else {
-          component.setState({
-            user: -1
+      .then(user => {
+        if (this.isMounted()) {
+          this.setState({
+            user
           });
         }
-      });
+      })
+      .catch(err =>
+        this.setState({
+          user: -1
+        })
+      );
   },
   render: function () {
+    const { children } = this.props;
+    const { user } = this.state;
+
     var UserExists = (
       <div>
-      <Header />
-        <div className = "white"></div>
-        <div id = "User-Container">
-          <div id = "Main-User-Container">
-            <UserSubHead user={this.state.user}/>
-            {(
-              (typeof this.state.user !== 'undefined')
-              ? this.props.children && React.cloneElement(this.props.children, {
-                user: this.state.user
+        <Header />
+        <div className="white" />
+        <div id="User-Container">
+          <div id="Main-User-Container">
+            <UserSubHead user={user} />
+            {user != null ? (
+              children &&
+              React.cloneElement(children, {
+                user
               })
-                : <div>Loading...</div>)
-            }
+            ) : (
+              <div>Loading...</div>
+            )}
           </div>
         </div>
         <Footer />
       </div>
     );
+
     if (this.state.user === -1) {
       return (
         <div>
           <Header />
-          <div id = "User-Container">
-            <div id = "No-User">
-              <div className = "Search-Container">
-                <div className = "Search-Box">
-                  <img src="assets/graphics/test.svg" width = "150px"></img>
-                  <div className = "Intro-Content">
+          <div id="User-Container">
+            <div id="No-User">
+              <div className="Search-Container">
+                <div className="Search-Box">
+                  <img src="assets/graphics/test.svg" width="150px" />
+                  <div className="Intro-Content">
                     <p>
-                      This database began on February 2016, so we haven't picked up any edits by this user yet. If you've just made a contribution, it may take a few minutes to show up. Go do some mapping, then come back and try again!
+                      We couldn't find any edits by this user yet. If you've
+                      just made a contribution, it may take a few minutes to
+                      show up. Go do some mapping, then come back and try again!
                     </p>
                     <p>
-                      <Link to='/'>&larr; Search for another user</Link>
+                      <Link to="/">&larr; Search for another user</Link>
                     </p>
                     <p>
-                      We also provide instructions if you would like to learn how to contribute to OpenStreetMap & MissingMaps!
+                      We also provide instructions if you would like to learn
+                      how to contribute to OpenStreetMap & Missing Maps!
                     </p>
                     <a href="http://www.missingmaps.org/contribute/">
-                    <div className = "button btn-grn">
-                      Contribute
-                    </div></a>
+                      <div className="button btn-grn">Contribute</div>
+                    </a>
                   </div>
                 </div>
               </div>
@@ -99,8 +94,8 @@ export default React.createClass({
           <Footer />
         </div>
       );
-    } else {
-      return (UserExists);
     }
+
+    return UserExists;
   }
 });
