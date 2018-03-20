da1848c2808f642ed8c3cd0369d6949a501eff2a
Seth Fitzsimmons <seth@mojodna.net>
2018-03-20 11:46:51 -0700

    Refactor edits box to show all hashtags
    This makes hashtag handling more gracefully now that we're tracking
    edits that don't necessarily have hashtags attached.

--- src/components/Recent.js
+++ src/components/Recent.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import moment from 'moment';
+import React from "react";
+import moment from "moment";
 
-export default (props) => {
-  if (!props.data) { return <div>Loading...<div></div></div>; }
+const pluralSuffix = num => (num === 1 ? "" : "s");
 
-  var data = props.data;
-  var latest = data.latest;
-  var latestTotal = Number(latest.road_count_add) +
+export default props => {
+  const { data, data: { latest } } = props;
+
+  if (data == null) {
+    return (
+      <div>
+        Loading...<div />
+      </div>
+    );
+  }
+
+  const latestTotal =
+    Number(latest.road_count_add) +
     Number(latest.road_count_mod) +
     Number(latest.building_count_add) +
     Number(latest.building_count_mod) +
     Number(latest.waterway_count_add) +
     Number(latest.poi_count_add);
 
-  var last_edit_time = latest.created_at;
-  var last_edit_day = moment(last_edit_time).format('dddd');
-  var last_edit_minutes = moment(last_edit_time).format('h:mm');
-  var is_plural = '';
-
-  test_plural(latestTotal);
-
-  function test_plural (num) {
-    if (num === 1) {
-      is_plural = '';
-    } else {
-      is_plural = 's';
-    }
-  }
+  const lastEditDay = moment(latest.created_at).format("dddd");
+  const lastEditMinutes = moment(latest.created_at).format("h:mm");
+  const lastHashtags = latest.hashtags.map(x => `#${x.hashtag}`).join(", ");
 
   return (
-    <div className = "Split split-stats">
-      <div className = "sub-section">
-        <h3 className = "descriptor">Latest Edit</h3>
-        <div className = "Recent-Edit-Box">
-          <span className = "EmphasizedNumber">{latestTotal.toLocaleString()}</span>
-          <div className = "Recent-Edit-Sidebar">
-            <p>edit{is_plural} made</p>
-            <p>{last_edit_day} at {last_edit_minutes}</p>
-            <p>#{data.latest.hashtags[0].hashtag}</p>
+    <div className="Split split-stats">
+      <div className="sub-section">
+        <h3 className="descriptor">Latest Edit</h3>
+        <div className="Recent-Edit-Box">
+          <span className="EmphasizedNumber">
+            {latestTotal.toLocaleString()}
+          </span>
+          <div className="Recent-Edit-Sidebar">
+            <p>tracked edit{pluralSuffix(latestTotal)} made</p>
+            <p>
+              {lastEditDay} at {lastEditMinutes}
+            </p>
+            <p>{lastHashtags}</p>
           </div>
         </div>
       </div>
