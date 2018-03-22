e1322877cb10b5a5ea66d200e89ea18108e8d421
Seth Fitzsimmons <seth@mojodna.net>
2018-03-22 15:53:50 -0700

    Changeset choropleth

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,6 +6,21 @@ import L from 'leaflet';
 import centroid from 'turf-centroid';
 import polygon from 'turf-polygon';
 
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+
+function getOpacity(d) {
+    return d > 10 ? 0.7 : 0;
+}
+
 export default React.createClass({
   getInitialState: function () {
     return {
@@ -14,13 +29,35 @@ export default React.createClass({
       kmStatsStyle: {clear: 'none'}
     };
   },
+  style: function (feature) {
+    const { data: { country_list: countries } } = this.props;
+
+    const count = countries[feature.properties.adm0_a3];
+
+    return {
+      fillColor: getColor(count),
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      fillOpacity: getOpacity(count)
+    };
+  },
   componentDidMount: function () {
-    var map = L.map('map', {minZoom: 2}).setView(this.state.position, 2);
+    var map = L.map('map', {minZoom: 1}).setView(this.state.position, 1);
     map.attributionControl.setPrefix('');
 
     L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1Ijoic3RhdGVvZnNhdGVsbGl0ZSIsImEiOiJlZTM5ODI5NGYwZWM2MjRlZmEyNzEyMWRjZWJlY2FhZiJ9.omsA8QDSKggbxiJjumiA_w.', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    fetch("https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson").then(rsp => rsp.json()).then(countries => {
+      L.geoJson(countries, { style: this.style }).addTo(map);
+
+      this.setState({
+        countries
+      })
+    });
+
 
     // force km stats to clear left if too large for container
     var roadCountLength = Number(this.props.data.total_road_count_add).toFixed().length;
@@ -33,27 +70,45 @@ export default React.createClass({
       map: map
     });
   },
+  componentWillReceiveProps: function(props) {
+  },
   render: function () {
     var user = this.props.data;
+    const { countries } = this.state;
 
-    var countries = R.reverse(R.sortBy(R.prop(1), R.toPairs(user.country_list)));
+    let countryList;
+
+    if (countries != null) {
+      countryList = Object.keys(user.country_list)
+        .map(x => {
+          const feature = countries.features.find(c => c.properties.adm0_a3 === x);
+
+          if (feature != null) {
+            return [feature.properties.name_long, user.country_list[x]];
+          }
+
+          return [x, user.country_list[x]]
+        });
+    } else {
+      countryList = Object.keys(user.country_list).map(x => [x, user.country_list[x]]);
+    }
+
+    const countryCounts = countryList.sort((a, b) => b[1] - a[1]);
     var changesetCount = Number(user.changeset_count);
 
     var total =
-      Number(user.total_road_count_add) +
-      Number(user.total_road_count_mod) +
-      Number(user.total_building_count_add) +
-      Number(user.total_building_count_mod) +
-      Number(user.total_waterway_count_add) +
-      Number(user.total_poi_count_add);
-
-    var hashtag = 'http://missingmaps.org/leaderboards/#/' + user.hashtags[hashtag];
+      user.total_road_count_add +
+      user.total_road_count_mod +
+      user.total_building_count_add +
+      user.total_building_count_mod +
+      user.total_waterway_count_add +
+      user.total_poi_count_add;
 
     // Round km calculation depending on length
-    var total_buildings = Number(user.total_building_count_add) + Number(user.total_building_count_mod);
-    var total_road_km = Number(user.total_road_km_add).toFixed(1);
+    var total_buildings = user.total_building_count_add + user.total_building_count_mod;
+    var total_road_km = user.total_road_km_add.toFixed(1);
     total_road_km = (total_road_km.length > 4) ? Math.round(total_road_km) : total_road_km;
-    var total_waterway_km = Number(user.total_waterway_km_add).toFixed(1);
+    var total_waterway_km = user.total_waterway_km_add.toFixed(1);
     total_waterway_km = (total_waterway_km.length > 4) ? Math.round(total_waterway_km) : total_waterway_km;
 
     return (
@@ -152,7 +207,7 @@ export default React.createClass({
                     <th><span>Countries most mapped</span></th>
                     <th></th>
                   </tr>
-                  {R.take(11, countries).map(function (country) {
+                  {R.take(11, countryCounts).map(function (country) {
                     var countryName = country[0];
 
                     if (country[0] === 'Democratic Republic of the Congo') {
